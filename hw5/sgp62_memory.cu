c8b87227e1a41087c2bb33a78a919ec8668142f5
Stefen Gerrit Pegels <sgp62@en-openmpi04.ece.cornell.edu>
2021-04-14 17:49:52 -0400

    working on hw5

--- hw5/sgp62_memory.cu
+++ hw5/sgp62_memory.cu
@@ -1,4 +1,5 @@
 //Stefen Pegels, sgp62
+// /usr/local/cuda-11.2/bin/nvcc -arch=compute_35 -o sgp62_memory.out sgp62_memory.cu
 /* Assignment 5, part I                                        */
 /* add two vectors in CUDA, 1D grid, 1D block configuration    */
 /* uses CUDA pagable memory allocations                        */
@@ -32,7 +33,7 @@ __global__ void vec_add (int *d_a, int *d_b, int *d_c, int size)
 
 int main() {
     int size = 1 << 28;                // length of vectors to be added
-    int block_size = 128;              // block size, try size 256 too
+    int block_size = 256;              // block size, try size 256 too
     int size_int = sizeof(int) * size;
 
 // variables to record times by different methods
@@ -50,6 +51,64 @@ int main() {
     cudaEventCreate(&cstart);
     cudaEventCreate(&cstop);
 
+    // start clock() timer
+    start = clock();
+
+    // start timing CUDA events from this marker point    
+    cudaEventRecord(cstart,0);
+/*
+//************* pinned memory **********
+// host
+  int *h_a1, *h_b1, *h_c1;
+  cudaMallocHost((int **)&h_a1, size_int);
+  cudaMallocHost((int **)&h_b1, size_int);
+  cudaMallocHost((int **)&h_c1, size_int);
+  for (int i = 0; i < size; i++) {
+    h_a1[i] = 1; h_b1[i] = 2;
+  }
+  memset(h_c1, 0, size);
+// device
+  int *d_a1, *d_b1, *d_c1;
+  cudaMalloc((int **)&d_a1, size_int);
+  cudaMalloc((int **)&d_b1, size_int);
+  cudaMalloc((int **)&d_c1, size_int);
+  cudaMemcpy(d_a1, h_a1, size_int, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b1, h_b1, size_int, cudaMemcpyHostToDevice);
+*/
+
+//*************** Mapped memory (zero-copy memory) ************
+// host
+  int *h_a2, *h_b2, *h_c2;
+  cudaHostAlloc((int **)&h_a2, size_int, cudaHostAllocMapped);
+  cudaHostAlloc((int **)&h_b2, size_int, cudaHostAllocMapped);
+  cudaHostAlloc((int **)&h_c2, size_int, cudaHostAllocMapped);
+  for (int i = 0; i < size; i++) {
+    h_a2[i] = 1; h_b2[i] = 2;
+  }
+  memset(h_c2, 0, size);
+
+// device
+  int *d_a2, *d_b2, *d_c2;
+  cudaHostGetDevicePointer((int **)&d_a2, (int *)h_a2, 0);
+  cudaHostGetDevicePointer((int **)&d_b2, (int *)h_b2, 0);
+  cudaHostGetDevicePointer((int **)&d_c2, (int *)h_c2, 0);
+
+
+
+//************** Unified memory **************************
+//   int *a, *b, *c;
+
+//  cudaMallocManaged((int **)&a, size_int);
+//  cudaMallocManaged((int **)&b, size_int);
+//  cudaMallocManaged((int **)&c, size_int);
+
+//  for (int i = 0; i < size; i++) {
+//    a[i] = 1; b[i] = 2;
+//  }
+//  memset(c, 0, size);
+
+
+/*
  //Host pagable memory allocation
     int *h_a, *h_b, *h_c;
     h_a = (int *)malloc(size_int); 
@@ -62,12 +121,6 @@ int main() {
     }
     memset(h_c, 0, size);
 
-// start clock() timer
-    start = clock();
-
-// start timing CUDA events from this marker point    
-    cudaEventRecord(cstart,0);
-
 //Device memory initialization
     int *d_a, *d_b, *d_c;
     cudaMalloc((int **)&d_a,size_int); 
@@ -77,29 +130,34 @@ int main() {
 //Host to device input data transfer
     cudaMemcpy(d_a, h_a, size_int, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, h_b, size_int, cudaMemcpyHostToDevice);
-  
+*/
 //Kernel launch
     dim3 block(block_size);
     dim3 grid(size/block.x);
-    vec_add <<< grid, block >>> (d_a, d_b, d_c, size);
+    vec_add <<< grid, block >>> (h_a2, h_b2, h_c2, size); //Change for memory management type
  
 // CPU waits until device is done
     cudaDeviceSynchronize();
 
 //Device to host output data transfer
-    cudaMemcpy(h_c, d_c, size_int, cudaMemcpyDeviceToHost);
+    //cudaMemcpy(h_c2, d_c2, size_int, cudaMemcpyDeviceToHost); //INCLUDE for all except unified
 
     cudaEventRecord(cstop,0);
     cudaEventSynchronize(cstop);
 
     end = clock();
 
+    FILE *tp = NULL;            
+	  tp = fopen("mem_time.csv", "w");
+
     time = ((float)(end-start))/CLOCKS_PER_SEC;
     printf("CUDA via clock(): %10.3e secs\n",time);
+    fprintf(tp, "clock: %10.3e, ",time);
 
     cudaEventElapsedTime(&time,cstart,cstop);
     time = time/1000;
     printf("CUDA events timer: %10.3e secs\n",time);
+    fprintf(tp, "event: %10.3e, ",time);
 
     cudaEventDestroy(cstart); cudaEventDestroy(cstop);
 
@@ -108,61 +166,27 @@ int main() {
     Ttime = BILLION * (Tend.tv_sec - Tstart.tv_sec) + Tend.tv_nsec - Tstart.tv_nsec;
     TTtime = (double)Ttime/1000000000.0;
     printf("CPU clock_gettime = %10.3e secs\n", TTtime);
+    fprintf(tp, "CPU: %10.3e, ",TTtime);
 //  printf("CPU clock_gettime = %llu secs\n",(long long unsigned int) Ttime);
 
-    free(h_a); free(h_b); free(h_c);
-    cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+    	/* open file for writing timing results                                   */
+
+	  
+
+	  fclose(tp);
+    
+    
+    // free(h_a); free(h_b); free(h_c); //Pageable
+    // cudaFree(d_a); cudaFree(d_b); cudaFree(d_c); //Pageable
+    //cudaFreeHost(h_a1); cudaFreeHost(h_b1); cudaFreeHost(h_c1); //Pinned
+    //cudaFree(d_a1); cudaFree(d_b1); cudaFree(d_c1); //Pinned
+    cudaFreeHost(h_a2); cudaFreeHost(h_b2); cudaFreeHost(h_c2); //Mapped
+    cudaFree(d_a2); cudaFree(d_b2); cudaFree(d_c2); //Mapped
+    //cudaFree(a); cudaFree(b); cudaFree(c); //Unified
+    
 
     return 0;
 }
 
-/* 
-************* pinned memory **********
-// host
-  int *h_a1, *h_b1, *h_c1;
-  cudaMallocHost((int **)&h_a1, size);
-  cudaMallocHost((int **)&h_b1, size);
-  cudaMallocHost((int **)&h_c1, size);
-  for (int i = 0; i < size; i++) {
-    h_a1[i] = 1; h_b1[i] = 2;
-  }
-  memset(h_c1, 0, size);
-// device
-  int *d_a1, *d_b1, *d_c1;
-  cudaMalloc((int **)&d_a1, size);
-  cudaMalloc((int **)&d_b1, size);
-  cudaMalloc((int **)&d_c1, size);
-  cudaMemcpy(d_a1, h_a1, size, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_b1, h_b1, size, cudaMemcpyHostToDevice);
 
-*************** Mapped memory (zero-copy memory) ************
-// host
-  int *h_a2, *h_b2, *h_c2;
-  cudaHostAlloc((int **)&h_a2, size, cudaHostAllocMapped);
-  cudaHostAlloc((int **)&h_b2, size, cudaHostAllocMapped);
-  cudaHostAlloc((int **)&h_c2, size, cudaHostAllocMapped);
-  for (int i = 0; i < size; i++) {
-    h_a2[i] = 1; h_b2[i] = 2;
-  }
-  memset(h_c2, 0, size);
-
-// device
-  int *d_a2, *d_b2, *d_c2;
-  cudaHostGetDevicePointer((int **)&d_a2, (int *)h_a2, 0);
-  cudaHostGetDevicePointer((int **)&d_b2, (int *)h_b2, 0);
-  cudaHostGetDevicePointer((int **)&d_c2, (int *)h_c2, 0);
-
-************** Unified memory **************************
-  int *a, *b, *c;
-
- cudaMallocManaged((int **)&a, size);
- cudaMallocManaged((int **)&b, size);
- cudaMallocManaged((int **)&c, size);
-
- for (int i = 0; i < size; i++) {
-   a[i] = 1; b[i] = 2;
- }
- memset(c, 0, size);
-
-*/
 
--- hw5/sgp62_mm.cu
+++ hw5/sgp62_mm.cu
@@ -1,21 +1,51 @@
+//Stefen Pegels sgp62
+// /usr/local/cuda-11.2/bin/nvcc -arch=compute_35 -o sgp62_mm.out sgp62_mm.cu
 /* naive matrix matrix multiplication of two N by N matrices    */
 /* 2D grid of 2D blocks where block is BLOCK_SIZE by BLOCK_SIZE */
 /* and grid is N/BLOCK_SIZE by BLOCK_SIZE                       */
 
 #include <stdio.h>
-#define BLOCK_SIZE 16
-#define N 4096
+#define BLOCK_SIZE 64 //Invariant: Tile size is always equal to block size
+#define N 16384
 
 __global__ void matrixMul(float *dev_A, float * dev_B, float * dev_C, int n)
 {
 	float partial = 0.0;
 	int k;
-	int i = blockIdx.y * blockDim.y + threadIdx.y; // Row i of C 
+	int i = blockIdx.y * blockDim.y + threadIdx.y; // Row i of C
 	int j = blockIdx.x * blockDim.x + threadIdx.x; // Column j of C
 
 	for (k = 0; k < n; k++)
 	  partial += dev_A[i*n + k]*dev_B[k*n + j];// not coalesed for B
   	dev_C[i*n + j] = partial;
+}
+
+__global__ void TilematrixMul(float *dev_A, float * dev_B, float * dev_C, int n, int k) //k is tile size
+{
+	__shared__ float cacheA[BLOCK_SIZE*BLOCK_SIZE]; //Shared mem allocation for tile
+	__shared__ float cacheB[BLOCK_SIZE*BLOCK_SIZE];
+
+	float partial = 0.0; //Temp for accumulating partial sum
+
+	int tix = threadIdx.x;
+	int tiy = threadIdx.y;
+	int bix = blockIdx.x;
+	int biy = blockIdx.y;
+
+	// k == blockDim.y == blockDim.x
+	int i = biy * k + tiy; // Row
+	int j = bix * k + tix; // Col
+
+	for(int m = 0; m < n/k; m++){ //Loop over tiles
+		cacheA[tiy*k + tix] = dev_A[i*n + (m*k + tix)]; //Loading a tile
+		cacheB[tiy*k + tix] = dev_B[(m*k*n + tiy*n) + j];
+		__syncthreads();
+		for(int l = 0; l < k; l++){
+			partial += cacheA[tiy*k + l] * cacheB[l*k + tix];//accumulate temp
+		}
+		__syncthreads();
+	}
+	dev_C[i*n + j] = partial; //Store in result C
 }
 
 int main(){
@@ -43,12 +73,14 @@ int main(){
 	cudaEventCreate( &stop ); 
 	
 // start timing
-        cudaEventRecord( start, 0 );
+    cudaEventRecord( start, 0 );
 
 	cudaMemcpy(dev_A, A, N*N*sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(dev_B, B ,N*N*sizeof(float), cudaMemcpyHostToDevice);
-	matrixMul<<<Grid, Block>>>(dev_A, dev_B, dev_C, N);
-        cudaMemcpy(C, dev_C, N*N*sizeof(float),cudaMemcpyDeviceToHost);
+	//matrixMul<<<Grid, Block>>>(dev_A, dev_B, dev_C, N);
+	TilematrixMul<<<Grid, Block>>>(dev_A, dev_B, dev_C, N, BLOCK_SIZE);
+    
+	cudaMemcpy(C, dev_C, N*N*sizeof(float),cudaMemcpyDeviceToHost);
 // stop timing
 	cudaEventRecord( stop, 0 );
 
@@ -61,6 +93,16 @@ int main(){
 // resolution is 0.5 micro second, but milliseconds are reported
 	printf(" %dx%d matrix mult, time %10.3e ms\n", N,N,elapsedTime);
 	printf("flop rate %10.3e\n",NN*NN*NN/(elapsedTime/1000.0));
+
+	/* open file for writing timing results                                   */
+	FILE *tp = NULL;            
+	tp = fopen("tile_time.csv", "w");
+	fprintf(tp, "%d, %d, %10.3e, ",N, BLOCK_SIZE, elapsedTime);
+
+	fclose(tp);
+
+
+
     
     free(A); free(B); free(C);
     cudaFree(dev_A); cudaFree(dev_B); cudaFree(dev_C);
